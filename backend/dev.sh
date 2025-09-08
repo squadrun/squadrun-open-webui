dd0de0d07fc1e86d82e7421cc7d149dd0c29dbcd
Bittu <bittu.kumar@squadstack.com>
2025-09-08 13:50:05 +0530

    fixed issues

--- backend/dev.sh
+++ backend/dev.sh
@@ -1,4 +1,4 @@
 #export CORS_ALLOW_ORIGIN="http://localhost:5173"
 export PYTHONPATH=$PYTHONPATH:/home/ubuntu/squadrun-open-webui/backend
 PORT="${PORT:-8080}"
-uvicorn open_webui.main:app --port $PORT --host 0.0.0.0 --forwarded-allow-ips '*' --reload
+uvicorn open_webui.main:app --port $PORT --host 0.0.0.0 --forwarded-allow-ips '*'
